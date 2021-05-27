a92cba8f3648915dafc8eb06c27bb3600a4e0685
Juhi Aggarwal <juhiaggarwal@Juhis-MacBook-Pro.local>
2021-05-26 23:06:38 -0500

    multiple mapping

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -16,6 +16,9 @@ L.circle([34.0522, -118.2437], {
  }).addTo(myMap);
 
  L.marker([34.0522, -118.2437], {
+    radius: 100,
+    color: "black",
+    fillColor: "yellow"
  }).addTo(myMap);
 
  // We create the tile layer that will be the background of our map.
